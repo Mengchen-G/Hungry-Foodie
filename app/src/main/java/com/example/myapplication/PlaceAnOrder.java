302723e155adbe7dc62faf686f0a70debeb204d3
tianliwu <tiwu5322@colorado.edu>
2020-04-14 17:11:07 -0600

    created new Activities

--- app/src/main/java/com/example/myapplication/PlaceAnOrder.java
+++ app/src/main/java/com/example/myapplication/PlaceAnOrder.java
@@ -31,32 +31,34 @@ public class PlaceAnOrder extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.acticity_checkout);
 
+        Log.d(TAG, "onCreate: Started.");
+
         ArrayList<Order> orderList = new ArrayList<>();
         Restaurant rest1 = new AmericanRestaurant();
+
         //get the bundle
         Bundle bundle = getIntent().getExtras();
+
         //Extract the data
         String order_name = bundle.getString("order_name");
+        //use abstract factory pattern to get meal object
         Meal meal = rest1.createMeal(order_name);
         Order order = new Order(meal, 1);
+        orderList.add(order);
 
 
         ListView mListView = (ListView) findViewById(R.id.cart_list);
-        Log.d(TAG, "onCreate: Started.");
-//
-//        NextProcessBtn = (Button) findViewById(R.id.next_process_btn);
-//        txtTotalAmount = (TextView) findViewById(R.id.total_price);
-
-        orderList.add(order);
         OrderListAdapter adapter = new OrderListAdapter(this, R.layout.cart_items_layout, orderList);
         mListView.setAdapter(adapter);
+
+//        NextProcessBtn = (Button) findViewById(R.id.next_process_btn);
+//        txtTotalAmount = (TextView) findViewById(R.id.total_price);
 
 //        EditText quantity = (EditText) findViewById(R.id.cart_meal_quantity);
 //        int quant1 = Integer.parseInt(quantity.getText().toString());
 //        order.setQuantity(quant1);
 //        orderList.add(order);
-//        OrderListAdapter adapter1 = new OrderListAdapter(this, R.layout.cart_items_layout, orderList);
-//        mListView.setAdapter(adapter1);
+//
 
 
     }
