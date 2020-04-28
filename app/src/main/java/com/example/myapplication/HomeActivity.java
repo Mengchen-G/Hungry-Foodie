ceb2c6043e45e1725788a41147067714b24767b9
FieryDev <theFieryDev@gmail.com>
2020-04-28 11:00:25 -0600

    Greeting message based on time added

--- app/src/main/java/com/example/myapplication/HomeActivity.java
+++ app/src/main/java/com/example/myapplication/HomeActivity.java
@@ -9,6 +9,9 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 // Home page for customer after login
 public class HomeActivity extends AppCompatActivity {
@@ -23,7 +26,19 @@ public class HomeActivity extends AppCompatActivity {
         final User current_client = (User) data.getParcelable("current_client");
         Log.d(TAG, "name: "  + current_client.getName() + " email: " + current_client.getEmail() + " password: " + current_client.getPassword());
 
-        String username = "Hi, " + current_client.getName();
+        Calendar c = Calendar.getInstance();
+        int timeOfDay = c.get(Calendar.HOUR_OF_DAY);
+
+        String message = "";
+        if(timeOfDay < 12){
+            message += "Good Morning ";
+        }else if(timeOfDay < 16){
+            message += "Good Afternoon ";
+        }else if(timeOfDay < 24){
+            message += "Good Evening ";
+        }
+
+        String username = message + current_client.getName() + "~";
         TextView user_welcome = findViewById(R.id.home_welcome);
         user_welcome.setText(username);
         user_welcome.setVisibility(View.VISIBLE);
